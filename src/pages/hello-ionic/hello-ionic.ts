8e6f33a640dbd46c18ad7f0b511311cb1cca5e12
ZSY <ZSY@.com>
2018-03-12 15:24:57 +0800

    买一买瞧一瞧咯

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
 
+  items: string[]
+  constructor(public alertCtrl: AlertController) {
+    this.items = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+      'american-football', 'boat', 'bluetooth', 'build']
+  }
+  nice () {
+    let alert = this.alertCtrl.create({
+      title: '烟火里的尘埃',
+      subTitle: '能不能慢下来',
+      buttons: ['OK']
+    })
+    alert.present()
   }
 }

4c90f39cb14952bbff9c4edd0d741828656eb8d2
ZSY <ZSY@.com>
2018-03-13 10:25:37 +0800

    父子组件通信

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -6,18 +6,23 @@ import { AlertController } from 'ionic-angular';
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-
-  items: string[]
+  nices: string;
+  items: string[];
+  vovo: string;
   constructor(public alertCtrl: AlertController) {
-    this.items = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-      'american-football', 'boat', 'bluetooth', 'build']
+    this.items = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane', 'american-football', 'boat', 'bluetooth', 'build'];
+    this.nices = '真的帅的一P'
   }
   nice () {
     let alert = this.alertCtrl.create({
       title: '烟火里的尘埃',
       subTitle: '能不能慢下来',
       buttons: ['OK']
-    })
+    });
     alert.present()
+  }
+
+  Voted (vovo: string) {
+    this.vovo = vovo
   }
 }
